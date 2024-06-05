7cec19934464d4990961a71d6930e6d5f4be6687
Amy Babay <amy.e.babay@gmail.com>
2024-06-05 17:16:53 -0400

    e_attach_fd kind of works, but can't actually pass fd to the callback

--- src/refcell.rs
+++ src/refcell.rs
@@ -4,25 +4,27 @@ use std::vec;
 use std::mem;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::net::{ToSocketAddrs, Ipv4Addr};
 use mio;
 
 const DEFAULT_EVENTS_CAPACITY: usize = 1024;
+const MAX_MSG_SIZE: usize = 1024;
 
 struct TimedEvent {
-    callback: Option<Box<dyn FnOnce()>>,
+    callback: Option<Box<dyn FnOnce()>>, // using option here allows us to invalidate the callback (set to none) to prove we won't call it again, so FnOnce is sufficient
     register_instant: time::Instant,
     delta_time: time::Duration,
-    done: bool,
 }
 
 struct FdEvent {
-    callback: Box<dyn FnOnce()>,
+    callback: Box<dyn FnMut()>,
+    token: mio::Token,
 }
 
-enum Event {
-    Timed(TimedEvent),
-    Fd(FdEvent),
-    Exit,
+pub enum Priority {
+    LowPriority,
+    MediumPriority,
+    HighPriority,
 }
 
 pub struct SpEvents {
@@ -47,9 +49,28 @@ impl SpEvents {
     }
 
     pub fn e_queue(&self, func: impl FnOnce() + 'static, delta_time: time::Duration) -> i32 {
-        let event = TimedEvent {callback: Some(Box::new(func)), register_instant: time::Instant::now(), delta_time: delta_time, done: false };
+        let event = TimedEvent {callback: Some(Box::new(func)), 
+                                register_instant: time::Instant::now(),
+                                delta_time: delta_time};
         self.timed_events.borrow_mut().push(event);
         0
+    }
+
+    pub fn e_attach_fd<S>(&self, source: &mut S, interest: mio::Interest, func: impl FnMut() + 'static, priority: Priority) -> io::Result<()>
+    where S: mio::event::Source + ?Sized,
+    {
+        let mut fd_events = self.fd_events.borrow_mut();
+        let event_count = fd_events.len();
+        if event_count == DEFAULT_EVENTS_CAPACITY {
+            return Err(io::Error::new(io::ErrorKind::Other, "Maximum number of FD events already registered"))
+        }
+
+        let token = mio::Token(event_count);
+        let event = FdEvent {callback: Box::new(func),
+                             token: token};
+
+        fd_events.push(event);
+        self.poll.borrow_mut().registry().register(source, token, interest)
     }
 
     pub fn e_exit_events(&self) {
@@ -91,8 +112,16 @@ impl SpEvents {
         Some(min_timeout)
     }
 
-    pub fn e_handle_events(&self) {
+    fn get_fd_event_by_token<'a>(&'a self, fd_events: &'a mut vec::Vec::<FdEvent>, token: mio::Token) -> Option<&mut FdEvent> {
+        for event in fd_events.iter_mut() {
+            if event.token == token {
+                return Some(event)
+            }
+        }
+        None
+    }
 
+    pub fn e_handle_events(&self) {
         let mut mio_events = mio::Events::with_capacity(DEFAULT_EVENTS_CAPACITY);
 
         loop {
@@ -110,6 +139,11 @@ impl SpEvents {
                 }
             }
 
+            // Check whether we should exit
+            if *self.exit_events.borrow() {
+                return;
+            }
+
             // Poll to check if we have events waiting for us.
             let timeout = self.get_next_timeout();
             if let Err(err) = self.poll.borrow_mut().poll(&mut mio_events, timeout) {
@@ -119,20 +153,16 @@ impl SpEvents {
                 std::process::exit(1);
             }
 
-            // Process all ready events. Note that spurious wakeups are possible, and that we are
+            // Process all ready fd events. Note that spurious wakeups are possible, and that we are
             // required to read until we get a WouldBlock error; otherwise, we are not guaranteed to be
             // notified the next time there is data ready to read.
-            /*
-             for event in events.iter() {
-                match event.token() {
-                    UDP_SOCKET => loop {
-                    } // end UDP recv loop
-
-                    // We only registered one token (UDP_SOCKET), so should never trigger on any other
-                    _ => unreachable!(),
-                } // end event match
+            let mut fd_events = self.fd_events.borrow_mut();
+            for event in mio_events.iter() {
+                let event_data = self.get_fd_event_by_token(&mut fd_events, event.token());
+                if let Some(ev) = event_data {
+                    (ev.callback)()
+                }
             } // end event iteration
-            */
 
             if mio_events.is_empty() {
                 println!("timeout...nothing recceived for {:?}", timeout);
@@ -142,6 +172,7 @@ impl SpEvents {
 }
 
 pub fn add(left: usize, right: usize) -> usize {
+    println!("adding {} + {}", left, right);
     left + right
 }
 
@@ -149,6 +180,30 @@ pub fn say_hello(events: Rc<SpEvents>) {
     println!("hello");
     let ev_clone = events.clone();
     events.e_queue(move || { say_hello(ev_clone); }, time::Duration::from_millis(5000));
+}
+
+pub fn send_msg(msg: &str, addr: &str) {
+    let socket = mio::net::UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0).into()).unwrap();
+    let dest_addr = addr.to_socket_addrs().unwrap().next().unwrap();
+    let bytes = socket.send_to(msg.as_bytes(), dest_addr).unwrap();
+    println!("Sent {bytes:?}  bytes: {}", msg);
+}
+
+pub fn receive_msg(socket: &mio::net::UdpSocket) {
+    let mut buf: [u8; MAX_MSG_SIZE] = [0; MAX_MSG_SIZE];
+    match socket.recv_from(&mut buf) {
+        Ok((bytes, from_addr)) => {
+            println!("Received {bytes:?} bytes from {}: {}",
+                     from_addr,
+                     std::str::from_utf8(&buf[0..bytes]).unwrap());
+        }
+        Err(err) if err.kind() == io::ErrorKind::WouldBlock => {
+            println!("Nothing to read! Would block");
+        }
+        Err(err) => {
+            println!("Error receiving! {}", err);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -162,7 +217,7 @@ mod tests {
     }
 
     #[test]
-    fn e_basics() {
+    fn e_timed() {
         let my_events = Rc::new(SpEvents::new().unwrap());
 
         let result = my_events.e_queue(|| { add(3, 4); }, time::Duration::from_millis(500));
@@ -178,4 +233,22 @@ mod tests {
 
         my_events.e_handle_events();
     }
+
+    #[test]
+    fn e_fd() {
+        let my_events = Rc::new(SpEvents::new().unwrap());
+        let mut socket =  mio::net::UdpSocket::bind("127.0.0.1:5555".parse().unwrap()).unwrap();
+
+        my_events.e_attach_fd(&mut socket, mio::Interest::READABLE, || { add(2, 5); }, Priority::HighPriority).unwrap();
+        my_events.e_queue(|| { send_msg("this is a test message", "127.0.0.1:5555"); }, time::Duration::from_secs(2));
+        my_events.e_queue(|| { send_msg("this is another test message", "127.0.0.1:5555"); }, time::Duration::from_secs(7));
+
+        let ev_clone = my_events.clone();
+        let result = my_events.e_queue(move || { ev_clone.e_exit_events(); }, time::Duration::from_secs(21));
+        assert_eq!(result, 0);
+
+
+        my_events.e_handle_events();
+    }
+
 }
